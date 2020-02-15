773f3c252048f310c217ff849f20abcb752dfc7c
Nabin <nabin123456j@gmail.com>
2020-02-15 19:48:01 +0545

    adding active red show signal  player in game

--- nabin.js
+++ nabin.js
@@ -10,10 +10,9 @@ var player1 , player2;
    var  arr=[];
     var  testarr;
     var nameentercheck;
+var c1,c2,c3,c4,c5,c6,c7,c8,c9;
+
   fun();
-
-
-
 
   function fun(){
       console.log(arr.length)
@@ -22,9 +21,10 @@ cbk=1;
       arr=[];
     round =0 ;
     activeplayer =0;
+    console.log(activeplayer)
       tick = "X";
       console.log("hello world");
-       document.querySelector('.win').classList.remove('winning');
+     document.querySelector('.win').classList.remove('winning');
      document.querySelector('.win').classList.remove('winner');
 
      document.querySelector('.pvs').textContent= '';
@@ -41,6 +41,10 @@ cbk=1;
     document.getElementById('player1name').value='';
     document.getElementById('player2name').value='';
 
+ document.querySelector('.lol1').classList.remove("round");
+ document.querySelector('.lol1').classList.remove("round");
+ c1=0;c2=0;c3=0;c4=0;c5=0;c6=0;c7=0;c8=0;c9=0;
+
 
 
     };
@@ -49,24 +53,30 @@ cbk=1;
 
     document.querySelector('#nextpage').addEventListener('click', function(){
         //test();
-        player1 = (document.getElementById('player1name').value).toUpperCase();
-        player2=(document.getElementById('player2name').value).toUpperCase(); 
-            console.log(typeof(player2))
-          if(player2!=='' && player1!==''){
-          nameentercheck=1;
-          console.log('nametestcheck' , nameentercheck)
+        if(nameentercheck!==1){
+            player1 = (document.getElementById('player1name').value).toUpperCase();
+            player2=(document.getElementById('player2name').value).toUpperCase(); 
+                console.log(typeof(player2))
+              if(player2!=='' && player1!==''){
+              nameentercheck=1;
+              console.log('nametestcheck' , nameentercheck)
+    
+              }
+              if(nameentercheck==1){
+                // document.querySelector('.pvs').style.color = 'black';
+                document.querySelector('.pvs').textContent= player1 + '  VS  ' + player2;
+               document.querySelector('.lol1').classList.add("round");
+    
+    
+                if(cbk%2==0){
+                    document.querySelector('.pvs').style.color = 'white';
+                }else{
+                document.querySelector('.pvs').style.color = 'black';
+            }
+        }
+        
+        };
 
-          }
-          if(nameentercheck==1){
-            // document.querySelector('.pvs').style.color = 'black';
-            document.querySelector('.pvs').textContent= player1 + '   VS    ' + player2;
-        }
-        if(cbk%2==0){
-            document.querySelector('.pvs').style.color = 'white';
-        }else{
-        document.querySelector('.pvs').style.color = 'black';
-           
-        }
          
     })
 
@@ -75,16 +85,21 @@ cbk=1;
 
 function nextplayer(){
  activeplayer===0 ? activeplayer = 1 : activeplayer=0;
-
+console.log(activeplayer)
  if(activeplayer==0){
       tick = 'X';
- document.querySelector('.pvs').textContent= player1 + '  turns ' + tick;
+//  document.querySelector('.pvs').textContent= player1 + '  turns ' + tick;
+
 
  }else{
       tick = 'O';
- document.querySelector('.pvs').textContent= player2 + '  turns' + tick ;
+//  document.querySelector('.pvs').textContent= player2 + '  turns' + tick ;
 
  }
+ document.querySelector('.lol1').classList.toggle("round");
+ document.querySelector('.lol2').classList.toggle("round");
+
+
 
 
 }
@@ -171,7 +186,6 @@ function winningshow(val){
     if(val==='draw'){
         document.querySelector('.win').textContent = val + '!!!';
         document.querySelector('.pvs').textContent= 'HOld !!!';
-
         document.querySelector('.win').classList.add('winning');
         document.querySelector('.win').classList.add('winner');
 
@@ -186,12 +200,14 @@ function winningshow(val){
 
 
         }
+       
         document.querySelector('.win').classList.add('winning');
         document.querySelector('.win').classList.add('winner');
         dc++;
     }
-
-
+    c1=10;c2=10;c3=01;c4=10;c5=10;c6=10;c7=10;c8=10;c9=10;
+    document.querySelector('.lol1').classList.remove("round");
+    document.querySelector('.lol2').classList.remove("round");
 }
 
 
@@ -228,89 +244,123 @@ function usernameerror(){
 
 
 document.querySelector('#bu1').addEventListener('click' , function(){
-    if(nameentercheck===1){
-        document.getElementById('bu1').textContent= tick;
-        console.log("i ma clicked")
-        nextplayer();
-        check();
-    }else{
-        usernameerror();   
-       }
+    if(c1===0){
+        if(nameentercheck===1){
+            document.getElementById('bu1').textContent= tick;
+            console.log("i ma clicked")
+            nextplayer();
+            check();
+            c1++;
+        }else{
+            usernameerror();   
+           }
+           
+    }
    
 });
 document.querySelector('#bu2').addEventListener('click' , function(){
-    if(nameentercheck===1){
-    document.getElementById('bu2').textContent= tick;
-    nextplayer();
-    check();
-    }else{
-     usernameerror();
+    if(c2===0){
+        if(nameentercheck===1){
+            document.getElementById('bu2').textContent= tick;
+            nextplayer();
+            check();
+            c2++
+            }else{
+             usernameerror();
+            }
     }
+    
 });
 document.querySelector('#bu3').addEventListener('click' , function(){
-    if(nameentercheck===1){
-    document.getElementById('bu3').textContent= tick;
-    nextplayer();
-    check();
-    }else{
-        usernameerror();   
-       }
-
+    if(c3===0){
+        if(nameentercheck===1){
+            document.getElementById('bu3').textContent= tick;
+            nextplayer();
+            check();
+            c3++;
+            }else{
+                usernameerror();   
+               }
+        
+    }
+ 
 });
 document.querySelector('#bu4').addEventListener('click' , function(){
-    if(nameentercheck===1){
-    document.getElementById('bu4').textContent= tick;
-    nextplayer();
-    check();
-    }else{
-        usernameerror();   
-       }
+    if(c4===0){
+        if(nameentercheck===1){
+            document.getElementById('bu4').textContent= tick;
+            nextplayer();
+            check();
+            c4++;
+            }else{
+                usernameerror();   
+               }
+    } 
 
 });
 document.querySelector('#bu5').addEventListener('click' , function(){
-    if(nameentercheck===1){
-        document.getElementById('bu5').textContent= tick;
-    nextplayer();
-    check();
-    }else{
-        usernameerror();   
-       }
+    
+    if(c5===0){    
+        if(nameentercheck===1){
+            document.getElementById('bu5').textContent= tick;
+        nextplayer();
+        check();
+        c5++;
+        }else{
+            usernameerror();   
+           }};
+
 });
 document.querySelector('#bu6').addEventListener('click' , function(){
+        
+    if(c6===0){    
     if(nameentercheck===1){
-    document.getElementById('bu6').textContent= tick;
-    nextplayer();
-    check();
-    }else{
-        usernameerror();   
-       }
+        document.getElementById('bu6').textContent= tick;
+        nextplayer();
+        check();
+        c6++;
+        }else{
+            usernameerror();   
+           }
+       };
 });
 document.querySelector('#bu7').addEventListener('click' , function(){
-    if(nameentercheck===1){
-    document.getElementById('bu7').textContent= tick;
-    nextplayer();
-    check();
-    }else{
-        usernameerror();   
-       }
+    if(c7===0){
+        if(nameentercheck===1){
+        document.getElementById('bu7').textContent= tick;
+        nextplayer();
+        check();
+        c7++;
+        }else{
+            usernameerror();   
+           }};
+    
 });
 document.querySelector('#bu8').addEventListener('click' , function(){
-    if(nameentercheck===1){
-    document.getElementById('bu8').textContent= tick;
-    nextplayer();
-    check();
-    }else{
-        usernameerror();   
-       }
+    if(c8===0){ 
+        if(nameentercheck===1){
+        document.getElementById('bu8').textContent= tick;
+        nextplayer();
+        check();
+        c8++;
+        }else{
+            usernameerror();   
+           }
+        };
+  
 });
 document.querySelector('#bu9').addEventListener('click' , function(){
+    if(c9===0){   
     if(nameentercheck===1){
-    document.getElementById('bu9').textContent= tick;
-    nextplayer();
-    check();
-    }else{
-        usernameerror();   
-       }
+        document.getElementById('bu9').textContent= tick;
+        nextplayer();
+        check();
+        c9++;
+        }else{
+            usernameerror();   
+           }
+    };
+
 });
 document.getElementById('resetall').addEventListener('click', fun);
 
@@ -339,11 +389,10 @@ function nightmodeoff(){
     document.querySelector('#nextpage').style.color = 'black';
     document.querySelector('#nextpage').style['border-color']= 'black';
     if(nameentercheck!==1){
-        document.querySelector('.pvs').style.color = 'RED';
+        document.querySelector('.pvs').style.color = 'red';
     }else{
         document.querySelector('.pvs').style.color = 'black';
-
     }
-    document.querySelector('.winner').style.color = 'White';
+    document.querySelector('.winner').style.color = 'black';
 
 }
